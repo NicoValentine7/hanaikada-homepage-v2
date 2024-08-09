ddbd9ed956a1b331a55259e99f3f9403c94766a6
Nico <50232024+NicoValentine7@users.noreply.github.com>
2024-08-09 17:28:02 +0900

    動いた

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import TechShareSummary from "./components/TechShareSummary"
+
+function App() {
+  return (
+    <div className="App">
+      <TechShareSummary />
+    </div>
+  )
+}
+
+export default App
